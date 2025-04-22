07afff385da757f3a41bb69895355ed480ec83e8
weezz <weezz2005@163.com>
2025-04-23 01:14:25 +0800

    update asm.py

--- sim/asm/asm.py
+++ sim/asm/asm.py
@@ -1,4 +1,18 @@
 from riscv_assembler.convert import *
-asm = input("type a file:")
-cnv = AssemblyConverter ('p', True, True)
+import argparse
+
+# 创建解析器
+parser = argparse.ArgumentParser(description="RISCV汇编器")
+
+# 添加参数
+parser.add_argument('--input', type=str, required=True, help='输入文件')
+parser.add_argument('--output', type=str, help='输出文件')
+
+# 解析参数
+args = parser.parse_args()
+asm = args.input
+cnv = AssemblyConverter ('p', False, True)
 result = cnv.convert(asm)
+if args.output != None:
+    cnv.output_mode='f'
+    result = cnv.convert(asm, args.output)
---
+++ sim/asm/usage.md
@@ -0,0 +1,10 @@
+## 进入虚拟环境
+```.\winriscvenv\bin\activate```
+这一步每个人可能不一样，运行自己的虚拟环境下的activate，一般在“虚拟环境目录/bin”或者“虚拟环境/Scripts”下
+## 运行python脚本
+```python3 ./asm.py --input [inputfilename] [--output outputfilename]```   
+或者   
+```python ./asm.py --input [inputfilename] [--output outputfilename]```  
+--input 是必须的，--output可选，但是后缀只能为.txt   
+
+![这是图片](./instruct.png "使用示例截图")
